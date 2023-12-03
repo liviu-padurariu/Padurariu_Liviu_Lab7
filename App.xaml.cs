fd332d07d9a5d72ee6cf04a00087c90c1d2f96d4
LiviuPadurariu <Cookie@COOKIE>
2023-12-03 15:30:52 +0100

    Add: Laboratorul8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace Padurariu_Liviu_Lab7
+﻿using Padurariu_Liviu_Lab7.Data;
+
+namespace Padurariu_Liviu_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
